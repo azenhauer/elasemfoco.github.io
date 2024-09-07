af6bdf5d5b4103a76cd8632a19195158fe71025d
azenhauer <80916116+azenhauer@users.noreply.github.com>
2024-09-07 15:47:01 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
+  resolve: {
+    alias: {
+      'react-router-dom': 'react-router-dom',
     },
-  }
+  },
   optimizeDeps: {
-    include: ['react-router-dom']
-  }
+    include: ['react-router-dom'],
+  },
 })
